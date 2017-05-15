5aef058c0ff0d9316c7efda4718cfd97a9b7ac40
maiosman <maiosman016@gmail.com>
2017-05-15 23:53:27 +0200

    admin edit

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Rails_MOOC_Project
 this is design and implement of a MOOC website similar to Coursera.
+
+ruby  : 2.2.3
+rails : 4.24
+used gems:
+1- gem 'devise'
+2- gem 'carrierwave', '~> 0.10.0'
+3- gem 'mini_magick', '~> 4.3'
+4- gem 'acts_as_votable', '~> 0.10.0'
+5- gem "ckeditor"
--- mooc/app/admin/user.rb
+++ mooc/app/admin/user.rb
@@ -1,4 +1,17 @@
 ActiveAdmin.register User do
   permit_params :name, :email, :gender, :image, :date_of_birth, :password, :password_confirmation, :role_id
 
+  form do |f|
+      f.inputs "New User" do
+
+        f.input :name
+        f.input :email
+        f.input :date_of_birth
+        f.input :image
+        f.input :gender
+        f.input :password
+        f.input :password_confirmation
+      end
+      f.actions
+    end
 end
